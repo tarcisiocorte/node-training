3ccbbb91ae1fb43bc9ee74937de06f0ae33a357a
tarcisiocorte <tarcisiocorte@gmail.com>
2017-08-16 14:25:38 -0300

    Property binding - how to use

--- Vuejs2/Vue2/src/main.js
+++ Vuejs2/Vue2/src/main.js
@@ -3,6 +3,13 @@ import Vue from 'vue'
 new Vue({
   el: '#application',
   data: {
-    titulo: "Minha primeira aplicação com Vue.js 2"
+    titulo: "Minha primeira aplicação com Vue.js 2",
+    bool: true,
+    numeroInteiro: 10,
+    numeroFloat: 20.30,
+    objeto: {
+      name: "Tarcísio Corte"
+    },
+    link: "https://localhost:8080"
   }
 })
