733d1cb91674f48042408bada382d435f1849752
tarcisiocorte <tarcisiocorte@gmail.com>
2017-09-05 10:58:06 -0300

    added user routes

--- crudMean/server/app.js
+++ crudMean/server/app.js
@@ -1,3 +1,15 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
+
+var port = 3000;
+var app = express();
+
+app.use('/', function(req, res){
+    res.send("I am up & running at port: " + port);
+});
+
+app.listen(port, function(){
+    console.log('Server started on port ' + port);
+});
